dcd8800e5285fecdfde0f46aaa050e4aa1fae625
Jun <wnsrl56@gmail.com>
2019-02-21 16:33:32 +0900

    init proejct

---
+++ src/Movie.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+  render() {
+    return <h1 className="Movie">this is Movie</h1>;
+  }
+}
+
+export default Movie;
