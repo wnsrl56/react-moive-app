0957d834547f7e29b21c5e2053bab4710f355c4d
Jun <wnsrl56@gmail.com>
2019-05-17 13:25:09 +0900

    arrange code line

--- src/App.tsx
+++ src/App.tsx
@@ -4,65 +4,71 @@ import Movie, { IMovieItem } from './Movie';
 import LoadingMask from './LoadingMask';
 
 interface IAppState {
-  movies: IMovieItem[];
+    movies: IMovieItem[];
 }
 
 class App extends Component<{}, IAppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      movies: []
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            movies: []
+        };
+    }
+    private _renderMovies = (list: IMovieItem[]) => {
+        return list.map((v: IMovieItem) => {
+            const {
+                title,
+                medium_cover_image: poster,
+                genres,
+                synopsis,
+                id
+            } = v;
+            return (
+                <Movie
+                    title={title}
+                    poster={poster}
+                    genres={genres}
+                    key={id}
+                    synopsis={synopsis}
+                />
+            );
+        });
     };
-  }
-  private _renderMovies = (list: IMovieItem[]) => {
-    return list.map((v: IMovieItem) => {
-      const { title, medium_cover_image: poster, genres, synopsis, id } = v;
-      return (
-        <Movie
-          title={title}
-          poster={poster}
-          genres={genres}
-          key={id}
-          synopsis={synopsis}
-        />
-      );
-    });
-  };
 
-  private _renderLoading = () => {
-    return (
-      <div className="Viewport">
-        <LoadingMask />
-      </div>
-    );
-  };
+    private _renderLoading = () => {
+        return (
+            <div className="Viewport">
+                <LoadingMask />
+            </div>
+        );
+    };
 
-  componentDidMount() {
-    this._getMovies();
-  }
+    componentDidMount() {
+        this._getMovies();
+    }
 
-  private _getMovies = async () => {
-    const movies = await this._callApi();
-    this.setState({ movies });
-  };
+    private _getMovies = async () => {
+        const movies = await this._callApi();
+        this.setState({ movies });
+    };
 
-  private _callApi = () => {
-    return fetch(
-      'https://yts.am/api/v2/list_movies.json?sort_by=download_count'
-    )
-      .then(response => response.json())
-      .then(json => json.data.movies)
-      .catch(error => console.log(error));
-  };
+    private _callApi = () => {
+        return fetch(
+            'https://yts.am/api/v2/list_movies.json?sort_by=download_count'
+        )
+            .then(response => response.json())
+            .then(json => json.data.movies)
+            .catch(error => console.log(error));
+    };
 
-  render() {
-    const { movies } = this.state;
-    return (
-      <div className="App">
-        {movies ? this._renderMovies(movies) : this._renderLoading()}
-      </div>
-    );
-  }
+    render() {
+        const { movies } = this.state;
+        return (
+            <div className="App">
+                {movies ? this._renderMovies(movies) : this._renderLoading()}
+            </div>
+        );
+    }
 }
 
 export default App;
